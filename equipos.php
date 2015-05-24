bc2438c16c1c0f7cf46d02860ae0e6e2b36c4a8f
Abarzua <luis.abarzua.ing@gmail.com>
2015-05-24 03:17:24 -0300

    Subir Proyecto
    Proyecto de Inventario

---
+++ equipos.php
@@ -0,0 +1,190 @@
+<?php
+//Proceso de conexión con la base de datos
+$conex = mysql_connect("localhost", "root", "1q2w3e4r5t")
+        or die("No se pudo realizar la conexion");
+    mysql_select_db("inventario",$conex)
+        or die("ERROR con la base de datos");
+
+//Iniciar Sesión
+session_start();
+
+//Validar si se está ingresando con sesión correctamente
+if (!$_SESSION){
+echo '<script language = javascript>
+alert("usuario no autenticado")
+self.location = "index.html"
+</script>';
+}
+$id_usuario = $_SESSION['id_user'];
+
+$consulta= "SELECT * FROM usuarios WHERE id_user='".$id_usuario."'"; 
+$resultado= mysql_query($consulta,$conex) or die (mysql_error());
+$fila=mysql_fetch_array($resultado);
+$apellidos = $fila['apellidos'];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Administracion Activos | EQUIPOS</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Navegacion</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php">HOME</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.php">QUIENES SOMOS?</a>
+                    </li>
+                    <li>
+                        <a href="services.php">SERVICIOS</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">CONTACTO</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">INVENTARIO <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="equipos.php">Notebook - PC's</a>
+                            </li>
+                            <li>
+                                <a href="impresoras.php">IMpresoras RICOH</a>
+                            </li>
+                            <li>
+                                <a href="proyectores.php">Data Show</a>
+                            </li>
+                            <li>
+                                <a href="captores.php">Captores de Datos</a>
+                            </li>
+                            <li>
+                                <a href="reportes.php">Reportes</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">USUARIOS <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="blog-home-1.php">Blog Home 1</a>
+                            </li>
+                            <li>
+                                <a href="blog-home-2.php">Blog Home 2</a>
+                            </li>
+                            <li>
+                                <a href="blog-post.php">Blog Post</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="mayuscula"><strong><? echo $_SESSION['nombres'];?></strong></span> <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="perfil.php">Perfil</a>
+                            </li>
+                            <li>
+                                <a href="sidebar.php"></a>
+                            </li>
+                            <li>
+                                <a href="faq.php">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="404.php">404</a>
+                            </li>
+                            <li>
+                                <a <a onclick="javascript: if(!confirm('¿De verdad quieres finalizar tu sesi&oacute;n?\n\nsi es as&iacute;, presiona ACEPTAR si no,\nentonces  presiona el bot&oacute;n CANCELAR')) return false"[<a href='desconectar_usuario.php']";">
+                                Cerrar Sesión</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- contenido de la pagina "equipos.php" -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Notebook - PC's
+                    <small>Administracion de Activos</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="home.php">Home</a>
+                    </li>
+                    <li class="active">Notebook - PC's</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Todos los derechos reservados &copy; Administración de Activos 2015</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
